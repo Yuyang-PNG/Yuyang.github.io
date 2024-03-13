ec9062e1a4816fcfe1e060f410a9696774ddb61f
Yuyang-PNG <140577150+Yuyang-PNG@users.noreply.github.com>
2024-03-13 18:01:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("loginBtn").addEventListener("click", function(e) {
+  e.preventDefault();
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  // 在此处添加登录验证逻辑
+  var usernameDb = localStorage.getItem("username");
+  var passwordDb = localStorage.getItem("password");
+  console.log(username);
+  if (username === usernameDb && password === passwordDb) {
+    window.location.href = "dashboard.html"; // 登录成功后跳转到仪表盘页面
+  } else {
+    alert("用户名或密码错误");
+  }
+});
